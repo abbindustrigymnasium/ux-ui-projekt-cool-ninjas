5c8c2a157491489df049a58546386985f9c6a56b
abbadanor <adam.nord@abbindustrigymnasium.se>
2021-08-24 13:52:11 +0200

    added beufy to main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import Home from './views/Home.vue'
 import Item from './views/Item.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Buefy)
 Vue.use(VueRouter)
 
 const routes = [{
