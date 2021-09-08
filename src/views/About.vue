fd3cb9b7bdbc6857639ecda07a696a804610e2eb
abbadanor <adam.nord@abbindustrigymnasium.se>
2021-09-08 13:00:57 +0200

    final

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <!-- TODO: add about page with staff data from database -->
+    <div class="about-container">
+      <h1>About us</h1>
+      <h2>Our story</h2>
+      <h4>A humble beginnning</h4>
+      <p>
+        In late 2015 we opened our first store in downtown L.A in a tiny bakery with barely enough space to fit the
+        staff.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
   name: 'About',
+  data() {
+    return {
+      staff: null,
+    };
+  },
+  async mounted() {
+    try {
+      this.staff = await Axios.get('http://localhost:3000/staff');
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.about-container {
+}
+</style>
