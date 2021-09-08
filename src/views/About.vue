44e126bc90d6a5ef2dbc496baeeabdacff82294a
abbadanor <adam.nord@abbindustrigymnasium.se>
2021-09-08 17:05:31 +0200

    added about and home

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,25 @@
         In late 2015 we opened our first store in downtown L.A in a tiny bakery with barely enough space to fit the
         staff.
       </p>
+      <p>
+        In 2017 we started to expand and even though we have many more stores all over the West Coast, the core idea has
+        stayed the same
+      </p>
+      <h1>Staff</h1>
+      <vs-card-group>
+        <vs-card :key="person.id" v-for="person in staff">
+          <template #title>
+            <h3>{{ person.name }}</h3>
+          </template>
+          <template #img>
+            <img :src="person.avatar" alt="" />
+          </template>
+          <template #text>
+            <p>{{ person.email }}</p>
+            <p>{{ person.phone }}</p>
+          </template>
+        </vs-card>
+      </vs-card-group>
     </div>
   </div>
 </template>
@@ -22,17 +41,40 @@ export default {
       staff: null,
     };
   },
-  async mounted() {
-    try {
-      this.staff = await Axios.get('http://localhost:3000/staff');
-    } catch (error) {
-      console.log(error);
-    }
+  mounted() {
+    Axios.get('http://localhost:3000/staff')
+      .then((response) => (this.staff = response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
 
 <style lang="scss">
-.about-container {
+.about {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .about-container {
+    width: 50vw;
+    background: #1e2023;
+    padding: 0 20px 0 20px;
+    border-radius: 5px 20px 20px 20px;
+    > * {
+      margin-bottom: 20px;
+    }
+  }
+}
+
+.vs-card__group-cards::-webkit-scrollbar {
+  height: 14px;
+}
+
+.vs-card__group-cards::-webkit-scrollbar-track {
+  background-color: #18191c;
+}
+
+.vs-card__group-cards::-webkit-scrollbar-thumb {
+  background: #4e4e4e;
+  border-radius: 7px;
 }
 </style>
--- src/views/Cake.vue
+++ src/views/Cake.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="cake" v-if="cake">
-      <h1>{{cake.title}}</h1>
-      <img :src="cake.image" alt="ja den är god">
-      <p>{{cake.detailDescription}}</p>
+    <div class="cake-container">
+      <h1>{{ cake.title }}</h1>
+      <img :src="cake.image" alt="ja den är god" />
+      <p>{{ cake.detailDescription }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import Axios from 'axios'
+import Axios from 'axios';
 
 export default {
   name: 'Cake',
   data() {
-      return {
-          cake: null
-      }
+    return {
+      cake: null,
+    };
   },
   mounted() {
     Axios.get('http://localhost:3000/cakes?id=' + this.$route.params.id)
       .then((response) => (this.cake = response.data[0]))
       .catch((error) => console.log(error));
   },
-}
+};
 </script>
 
 <style lang="scss">
+.cake {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    border-radius: 20px;
+  }
+  .cake-container {
+    width: 50vw;
+    background: #1e2023;
+    padding: 0 20px 0 20px;
+    border-radius: 5px 20px 20px 20px;
+    > * {
+      margin-bottom: 20px;
+    }
+  }
+}
 </style>
